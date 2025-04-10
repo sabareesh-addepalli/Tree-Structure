eb9c5ea6c9246ba017083536fa2d7819b6611adf
Sab Addepalli <Sabareesh@LAPTOP-H72HCCCO.(none)>
2025-04-10 23:52:49 +0530

    Added level 1 item and child logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+  const addFirstItem = () => {
+    setData([
+      {
+        label: "Parent",
+        child: [],
+      },
+    ]);
+  };
+  const addItem = (level, index) => {
+    console.log(level, index);
+    const updatedData = [...data];
+    updatedData[index + 1] = { label: "child", child: [] };
+    console.log(updatedData);
+    setData(updatedData);
+  };
+  const addChild = (level,index) => {
+    console.log(level,index);
+    const updatedData = [...data];
+    console.log(updatedData[index])
+    updatedData[index].child.push({ label: "child", child: [] })
+    console.log(updatedData);
+    setData(updatedData);
+  };
+  const renderChild = (children, level = 0) => {
+    if (children.length === 0) {
+      return <button onClick={addFirstItem}>add</button>;
+    }
+    return (
+      <>
+        {children.map((item, index) => (
+          <div>
+            <span>
+              <input value={item.label} className="item" />{" "}
+              <button onClick={() => addChild(level,index)}>add child</button>
+            </span>
+
+            {children.length === index + 1 && (
+              <button onClick={() => addItem(level, index)}>add</button>
+            )}
+            {item.child && item.child.length > 0 && (
+              <div className="childItem">
+                {renderChild(item.child, level + 1)}
+              </div>
+            )}
+          </div>
+        ))}
+      </>
+    );
+  };
+  return <div className="App">{renderChild(data)}</div>;
 }
 
 export default App;
